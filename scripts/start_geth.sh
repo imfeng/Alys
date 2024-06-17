c06613f2bf52b4751c86398397aa0c1cebe671f4
Nick Vincent <emailnjv@gmail.com>
2024-06-17 12:53:08 -0400

    Chore: Imported repo

---
+++ scripts/start_geth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# includes
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/utils/geth.sh
+
+trap stop_all_geth SIGINT
+
+if [[ -z "${NUM}" ]]; then
+    # when running dev mode (single node)
+    rm -rf ".alys/chain_db"
+    rm -rf ".alys/wallet"
+fi
+
+mkdir -p data/logs/
+
+NUM=${NUM:-0}
+start_geth $NUM
+tail -f "$PWD/data/logs/geth${NUM}.txt"
